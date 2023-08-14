2491a62ee124ca18e514fb90e24098c237396c8c
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2023-08-14 13:58:16 +0000

    fix make install

--- chatbot.py
+++ chatbot.py
@@ -4,3 +4,15 @@ import numpy as np
 import pickle as pk
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix
+from sklearn.naive_bayes import GaussianNB
+import re
+from nltk.stem.porter import PorterStemmer
+from sklearn.model_selection import train_test_split
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import np_utils
+from keras.models import load_model
+import tensorflow
+
