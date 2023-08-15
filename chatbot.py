855e6b3cb058b7cb52cfd1d3d257959f8211aee4
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2023-08-15 18:57:47 +0000

    Removed redundant tests

--- chatbot.py
+++ chatbot.py
@@ -107,3 +107,13 @@ def trainIntentModel():
 intent_model, intent_label_map = trainIntentModel()
 
 # Save the Intent model
+intent_model.save('saved_state/intent_model.h5')
+print("Intent model saved!")
+
+
+
+def trainEntityModel():
+    # Importing dataset and splitting into words and labels
+    dataset = pd.read_csv('datasets/data-tags.csv', names=["word", "label"])
+    X = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, 1].values
